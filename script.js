f20a8c6efd700831ea31b4c70bc96d05c57fb097
addering <79804512+addering@users.noreply.github.com>
2025-02-01 20:51:15 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const heading = document.querySelector("h1");
+  heading.style.color = "blue";
+  heading.textContent += " (Modificato con JavaScript!)";
+});
